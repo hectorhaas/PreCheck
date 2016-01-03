eae9c0a996585bee11ae537e695480e64d9110d4
hectorhaas <hectorhaas35@gmail.com>
2016-01-03 16:49:28 -0600

    finished GetBlurbsByCategory

--- PrecheckDataAccess/DA_Blurbs.cs
+++ PrecheckDataAccess/DA_Blurbs.cs
@@ -22,7 +22,7 @@ namespace PrecheckDataAccess
         public BM_Blurb getBlurbWithID(string blurbID)
         {
             BM_Blurb blurb = new BM_Blurb();
-            string query = "SELECT id, blurbText, categoryID FROM hectorhaas.precheck WHERE id = " + blurbID;
+            string query = "SELECT id, blurbText, categoryID FROM hectorhaas.precheckBlurbs WHERE id = " + blurbID;
             DataTable dt = DataAccess.getAnyDataTable(query);
             blurb.categoryID = dt.Rows[0]["categoryID"].ToString();
             blurb.blurbText = dt.Rows[0]["blurbText"].ToString();
@@ -32,7 +32,34 @@ namespace PrecheckDataAccess
         public List<BM_Blurb> getAllBlurbs()
         {
             List<BM_Blurb> blurbList = new List<BM_Blurb>();
-
+            //get data
+            string query = "SELECT id, blurbText, categoryID FROM hectorhaas.precheckBlurbs ORDER BY blurbText ASC";
+            DataTable dt = DataAccess.getAnyDataTable(query);
+            for (int i = 0;i<dt.Rows.Count;i++)
+            {
+                BM_Blurb tempBlurb = new BM_Blurb();
+                tempBlurb.id = dt.Rows[i]["id"].ToString();
+                tempBlurb.categoryID = dt.Rows[i]["categoryID"].ToString();
+                tempBlurb.blurbText = dt.Rows[i]["blurbText"].ToString();
+                blurbList.Add(tempBlurb);
+            }
+            return blurbList;
+        }
+        public List<BM_Blurb> getAllBlurbsByCategory(string categoryID)
+        {
+            List<BM_Blurb> blurbList = new List<BM_Blurb>();
+            //get data
+            string query = "SELECT id, blurbText, categoryID FROM hectorhaas.precheckBlurbs WHERE categoryID = " + categoryID+" ORDER BY blurbText ASC";
+            DataTable dt = DataAccess.getAnyDataTable(query);
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                BM_Blurb tempBlurb = new BM_Blurb();
+                tempBlurb.id = dt.Rows[i]["id"].ToString();
+                tempBlurb.categoryID = dt.Rows[i]["categoryID"].ToString();
+                tempBlurb.blurbText = dt.Rows[i]["blurbText"].ToString();
+                blurbList.Add(tempBlurb);
+            }
+            return blurbList;
         }
     }
 }
